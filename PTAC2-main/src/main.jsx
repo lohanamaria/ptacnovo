17a86b978be6e754ea9e783351735f0146c29946
maria maria <decarvalholohana@gmail.com>
2023-10-21 21:16:38 -0400

    projeto novo

---
+++ PTAC2-main/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+export function App() {
+  return (
+      <div>
+        <h1>Hello Word!</h1>
+      </div>
+
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
